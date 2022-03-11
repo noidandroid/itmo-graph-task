149d2abedad9491c8c09f1b05d25b4eb080e7bf4
noidandroid <48490323+noidandroid@users.noreply.github.com>
2022-03-11 17:40:22 +0300

    wip: base graph node

--- src/graphNode/models.py
+++ src/graphNode/models.py
@@ -2,3 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class Node(models.Model):
+    """Base graph node model"""
+    #id = models.IntegerField("Node id")
+
+    class Meta:
+        db_table = 'node'
+
+class Vector(Node):
+    """Vector node model"""
+   # valueList = models.JSONField("Vector container")
+
+class Operation(Node):
+    """Operation node model"""
+    name = models.CharField("Operation name", max_length=250)
